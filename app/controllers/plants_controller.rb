3c325403b8c7b8b92e081c789bd8cd3f109a7215
neivyx <janeivyhilario@gmail.com>
2020-09-25 14:17:14 -0400

    + new/index methods

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,28 @@
 class PlantsController < ApplicationController
+
+
+    def new
+        if params[:user_id] && @user = User.find_by_id(params[:user_id])
+          @plant = @user.plants.build
+        else
+          @plant = Plant.new
+        end
+        @plant.build_category
+      end
+
+    def index
+        if params[:user_id] && @user = User.find_by_id(params[:user_id])
+           @plants = @user.plants.alpha
+        else
+          @error = "That user doesn't exist" if params[:user_id]
+          @plants = Plant.alpha.includes(:category, :user)
+        end
+    
+       
+    end
+    
+
+
+
+
 end
