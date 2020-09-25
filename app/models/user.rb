f01a67a9f66a749f3e9b581741c6a76b3feb5478
neivyx <janeivyhilario@gmail.com>
2020-09-25 14:50:21 -0400

    + has_many plants/reviews/reviewedpl/password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :plants
+    has_many :reviews
+    has_many :reviewed_plants, -> { distinct }, through: :reviews, source: :plant
+    has_secure_password  #authenticate, validate password &/or password confirmation
+
 end
