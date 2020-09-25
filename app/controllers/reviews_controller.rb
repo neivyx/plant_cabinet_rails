c012c92bb00a367f7a3765cfe4cc08b4101c9c76
neivyx <janeivyhilario@gmail.com>
2020-09-25 17:20:22 -0400

    +add methods index,show,new

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,16 @@
 class ReviewsController < ApplicationController
+
+    def index
+        @plant = Plant.find(params[:plant_id])
+        @reviews = @plant.reviews
+      end
+      def show
+        @review = Review.find(params[:id])
+      end
+      def new
+        @plant = Plant.find(params[:plant_id])
+        @review = Review.new
+      end
+  
+
 end
