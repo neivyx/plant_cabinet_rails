b7066156e1ad6aa0d2df4d321621651e8033f302
neivyx <janeivyhilario@gmail.com>
2020-09-25 13:27:23 -0400

    +add rm false null

--- db/migrate/20200925165813_create_plants.rb
+++ db/migrate/20200925165813_create_plants.rb
@@ -6,7 +6,7 @@ class CreatePlants < ActiveRecord::Migration[6.0]
       t.string :location
       t.string :light_requirement
       t.string :watering
-      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :user, foreign_key: true
 
       t.timestamps
     end
