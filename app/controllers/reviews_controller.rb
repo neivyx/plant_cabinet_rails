45351d012e90a89635a1a56e18c3fc240e9a45f7
neivyx <janeivyhilario@gmail.com>
2020-09-25 17:21:17 -0400

    + add methods edit/update/create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,6 +11,27 @@ class ReviewsController < ApplicationController
         @plant = Plant.find(params[:plant_id])
         @review = Review.new
       end
+
+      def edit
+        @review = Review.find(params[:id])
+      end
+      def update
+        @review = Review.find(params[:id])
+        if @review.update(review_params)
+          redirect_to plant_reviews_path(@review.plant)
+        else
+          render :edit
+        end
+      end
+      def create
+        @review = current_user.reviews.build(**review_params, plant_id: params[:plant_id])
+        if @review.save
+          redirect_to redirect_to plant_reviews_path(@review.plant), notice: "Review successfully created."
+        else
+          render :new
+        end
+      end
+   
   
 
 end
