42e789df1fccdc8a4753e1344d52dab3519f48ef
neivyx <janeivyhilario@gmail.com>
2020-09-25 13:39:11 -0400

    +add new/create methods for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new
+      end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          session[:user_id] = @user.id
+          #redirect to the show page
+          redirect_to @user
+        else
+          render :new
+        end
+    end
+    
+
+
 end
