62839c7ec3aadb8e473166395f14d7c7547cbc16
neivyx <janeivyhilario@gmail.com>
2020-09-30 13:16:45 -0400

    +add valindates name email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,27 @@ class User < ApplicationRecord
     has_many :reviewed_plants, -> { distinct }, through: :reviews, source: :plant
     has_secure_password  #authenticate, validate password &/or password confirmation
 
+    validates :name, :email, presence: true
+
+    # def self.find_or_create_with_oauth(auth)
+    #     user = User.find_by_uid(auth["uid"]) || User.find_or_create_by_email(auth)
+    #     user.email = auth["info"]["email"]
+    #     user.uid = auth["uid"]
+    #     #  binding.pry
+    #     user.save
+    #     user
+    #   end
+
+    # def self.find_or_create_by_email(auth)
+    #     User.find_or_create_by(email: auth["info"]["email"]) do |user|
+    #       user.uid = auth["uid"]
+    #       user.password = SecureRandom.hex(20)
+    #       user.username = auth["info"]["name"]
+    #     end
+    #   end
+    
+    
+
 end
+
+
