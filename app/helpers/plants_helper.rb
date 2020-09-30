d20b7e479945913b039f318263636b454c74d696
neivyx <janeivyhilario@gmail.com>
2020-09-30 05:43:55 -0400

    +add review tag for method display

--- app/helpers/plants_helper.rb
+++ app/helpers/plants_helper.rb
@@ -1,11 +1,12 @@
 module PlantsHelper
 
+
     def display_plants
         if @user.plants.empty?
           tag.h2(link_to('No Plants yet - create a plant here', new_plant_path))
         else
           user = @user == current_user ? 'Your' : "#{@user.name}'s"
-          content_tag(:h2, "#{user} #{pluralize(@user.plants.count, 'Plant')}:")
+          review_tag(:h2, "#{user} #{pluralize(@user.plants.count, 'Plant')}:")
     
         end
       end
