1c7a2578f18aff7de2fedda1f28e6239073fb7f7
neivyx <janeivyhilario@gmail.com>
2020-09-30 13:46:35 -0400

    -rm notes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :plants
     has_many :reviews
     has_many :reviewed_plants, -> { distinct }, through: :reviews, source: :plant
-    has_secure_password  #authenticate, validate password &/or password confirmation
+    has_secure_password
 
     validates :name, :email, presence: true
 
